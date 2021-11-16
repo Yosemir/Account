ba043f6bfbe6c03927eb96aeb8cab3162dd59662
wualtervera <waltervera693@gmail.com>
2021-11-16 15:27:50 -0500

    First commit

---
+++ src/test/java/com/nttdata/acount/AcountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.acount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
